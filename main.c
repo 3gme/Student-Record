9c7199aa3914d668770cd717fb2f2110b429ed1e
mahmoud ezzat <mahmoud.ezzat1234123121@gmail.com>
2024-04-23 04:20:30 +0200

    "handle the login of the user"

--- main.c
+++ main.c
@@ -1,60 +1,215 @@
+/*---------------------START OF HASHS---------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define bool int
 #define true 1
-#define flase 0
+#define false 0
+#define tries 3
+/*---------------------END OF HASHS---------------------*/
 
-typedef struct student
-{
-    /* data */
+
+/*---------------------START OF STRUCTS---------------------*/
+typedef struct student{
+int id;
+char* pass;
+char* fullname[3];
+int age;
+char* gender;
+int total_grade;
 }student;
+/*---------------------END OF STRUCTS---------------------*/
 
-int Get_id()
+
+
+/*---------------------START OF FUNCTOINS---------------------*/
+char Case()                                         // determin if the user will go admin mode or user mode 
+{
+    printf("---------Welcome to our Student Record system---------\n");
+    printf("Will you go user or admin ('u' for user,'a' for admin): ");
+    char ch;
+    scanf("%c",&ch);
+    return ch;
+}
+
+int Get_id()                                        // get the id fron the user or the admin
 {
     int id;
-    //get the id
+    printf("ID: ");
+    scanf("%d",&id);
     return id;
 }
 
-int Get_grade()
+void Get_pass(char* pass)                           // get the pass from the user 
+{
+   printf("Password: ");
+   scanf("%s",pass);
+}
+
+bool Check_id(FILE *f,int id)                       // check if the inserted id is in the file or not 
+{
+    f =fopen("file.txt","r");
+    char* buffer = (char*) malloc(101);
+    fscanf(f,"%50[^\n]",buffer);          // to skip the first line.
+    int _id;
+    while(fscanf(f,"%d",&_id) != EOF)
+    {
+        if(_id == id)   
+        {
+            fclose(f);
+            return true;
+        }
+        else
+        {
+            fscanf(f,"%200[^\n]",buffer);          // to skip the rest of the line.
+        }
+    }
+    fclose(f);
+    return false;
+}
+
+int Get_grade()                                     // gets the grade to send it to the admin
 {
     int grade;
-    //
+    scanf("%d",&grade);
     return grade;
 }
 
-bool Check_id(FILE *f,int id) 
-{
-    //check if the id exists before
-    //true if not and flase if it exists
-}
-
-char Case()
-{
-    //determine if user or admin
-    //return char 'u' for user , 'a' for admin
-}
-
-int Show_option_Admin()
+int Show_option_User()                              // display the options for the user and return his choise 
 {
     int option ;
-    // show the admin options 
-    // take the option number in option and return it
+    bool flag = true;
+    int counter = 0  ; 
+    printf("1. View your record\n");
+    printf("2. Edit your password\n");
+    printf("3. Edit your name\n");
+    printf("enter the option's number: ");
+    while(flag && counter != tries)
+    {
+        if(counter != 0 ) printf("invalid option try again: ");    
+        scanf("%d",&option);
+        if(option > 0 && option < 4) flag = false;
+        else counter ++ ;
+    }
+    if(counter == tries)
+    {
+        printf("sorry wrong data");
+        return -1;
+    }
     return option; 
 }
 
-int Show_option_User()
+int Show_option_Admin()                             // display the optoins for the admin and return his choise 
 {
     int option ;
-    // show the user options 
-    // take the option number in option and return it
+    bool flag = true;
+    int counter = 0  ; 
+    printf("1. Add student record\n");
+    printf("2. Remove student record\n");
+    printf("3. View student record\n");
+    printf("4. View all records\n");
+    printf("5. Edit admin password\n");
+    printf("6. Edit student grade\n");
+    printf("enter the option's number: ");
+    while(flag && counter != tries)
+    {
+        if(counter != 0 ) printf("invalid option try again: ");    
+        scanf("%d",&option);
+        if(option > 0 && option < 7) flag = false;
+        else counter ++ ;
+    }
+    if(counter == tries)
+    {
+        printf("sorry wrong data");
+        return -1;
+    }
     return option; 
 }
 
+bool Check_user_pass(FILE *f , char* pass)          // check if the pass the user entered match the id(true) or not(false) 
+{
 
+    f =fopen("file.txt","r");
+    char* buffer = (char*) malloc(101);
+    fscanf(f,"%200[^\n]",buffer);          // to skip the first line.
+    char* _pass = (char*) malloc(20);
+    int id;
+    while(fscanf(f,"%d",&id)!= EOF)
+    {
+        fscanf(f," %s",_pass);
+        if(!strcmp(_pass,pass))  
+        {
+            fclose(f);
+            return true;
+        } 
+        else
+        {
+            fscanf(f,"%200[^\n]",buffer);          // to skip the rest of the line.
+
+        }
+    }
+    fclose(f);
+    return false;
+}
+
+bool Log_in(FILE* f)                                // handles the whole login operatoin 
+{
+    char* pass = (char*) malloc(25);
+    int counter = 3;
+    bool flag = false;
+
+    while(counter --)                   
+    {
+        int id = Get_id();
+        if(Check_id(f ,id)) 
+        {
+            flag = true;
+            break;
+        }
+        else 
+        {   
+            if(counter)
+                printf("invalid ID try again \n");
+        }
+    }
+
+    if(flag) 
+    {
+        counter = 3 ;
+        while(counter--)
+        {
+            Get_pass(pass);
+            if(Check_user_pass(f,pass)) 
+            {
+                free(pass);
+                return true;                
+            }
+            else if(counter)
+            {
+                printf("\awrong password try again\n");
+            }
+        }
+    }
+        if(counter == -1)                   // checking by -1 cuz when the while's postfix decrement     ...
+        {
+            printf("\asorry wrong data");
+            free(pass);
+            return false;
+        }
+
+}
+/*---------------------END OF FUNCTOINS---------------------*/
+
+
+
+/*---------------------START OF MAIN---------------------*/
 int main()
 {
     FILE *file;
     char ch = Case();
+    printf("\n");
+    /*--------------------------------START OF ADMIN--------------------------------*/
     if(ch == 'a' )
     {
         int pass ;
@@ -69,7 +224,7 @@ int main()
         case 1: // add a student record
         {
             int id = Get_id();
-            if (check_id( file ,id))      
+            if (Check_id( file ,id))      
             //ADD_new_record( file )                     (admin.h)  
             break;
         }
@@ -78,7 +233,7 @@ int main()
         case 2: // Remove a student record
         {
             int id = Get_id();
-            if (!check_id( file ,id))      
+            if (Check_id( file ,id))      
             //Remove_record( file , int id)               (admin.h)  
             break;
         }    
@@ -87,7 +242,7 @@ int main()
         case 3: // view student record
         {
             int id = Get_id();
-            if (!ckeck_id( file  , id))
+            if (Check_id( file  , id))
             //Show_record(file , id)                   (admin.h)  
             break;
         }
@@ -113,7 +268,7 @@ int main()
         case 6: // Edit studint grade
         {
             int id = Get_id();
-            if(!Check_id(file,id))
+            if(Check_id(file,id))
             {
             
             int new_grade = Get_grade();
@@ -130,18 +285,16 @@ int main()
             break;
         }
     }
+    /*--------------------------------END OF ADMIN--------------------------------*/
+
+
+
+    /*--------------------------------START OF USER--------------------------------*/
     else if(ch == 'u')
     {
-        int option = Show_option_User();
-        int id ; 
-        // get id
-        if(!Check_id(file ,id)) 
-        {
-            //int pass
-            //Get_pass 
-            //check_pass(pass)
-        }
+        if(!Log_in(file)) return 1;
         
+        int option = Show_option_User();
         switch (option)
         {
         case 1: // show your record
@@ -152,14 +305,14 @@ int main()
             {
                 int pass;
                 //get the new pass 
-                //Edit_pass(file,id,pass);
+                //Edit_pass(file,id,pass);          (user.c)
                 break;
             }
         
         case 3:
             {
                 char* new_name ;
-                //Edit_name(file,id,new_name)
+                //Edit_name(file,id,new_name)           (user.c)
             }
             
             break;
@@ -169,6 +322,16 @@ int main()
             break;
         }
     }
-    else printf("wrong data try agian");
+    /*--------------------------------END OF USER--------------------------------*/
+
+
+
+    /*--------------------------------START OF MISSING DATA--------------------------------*/
+    else
+    {
+        printf("wrong data try agian");
+    }
+    /*--------------------------------END OF MISSING DATA--------------------------------*/
     
 }
+/*---------------------END OF MAIN---------------------*/
